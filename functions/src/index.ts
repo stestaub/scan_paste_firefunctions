613a4a001285e42259256cda904e59a0888c1211
Stefan Staub <stefan.staub@pta.ch>
2020-09-11 13:16:58 +0200

    initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,38 @@
+import * as functions from 'firebase-functions';
+import * as firebaseAdmin from 'firebase-admin';
+
+firebaseAdmin.initializeApp();
+
+exports.requestScan = functions.database.ref('/request/{requestId}')
+    .onCreate((snapshot, context) => {
+        // Grab the current value of what was written to the Realtime Database.
+        const channel = snapshot.child("channel_id").val();
+        return firebaseAdmin.database().ref("/channels/" + channel).once('value').then(
+            (channelSnapshot) => {
+                return sendMessage(channelSnapshot.val(), context.params.requestId);
+            }
+        );
+    });
+
+function sendMessage(registrationToken: string, requestId: string){
+    const prio: ('high' | 'normal') = 'high';
+    const message = {
+        notification: {
+            title: "Scan Anfrage",
+            body: "Klicke auf die Benachrichtigung um den Scanner zu starten.",
+        },
+        data: {
+            request_id: requestId
+        },
+        android: {
+            ttl: 600,
+            notification: {
+                clickAction:  "OPEN_SCANNER",
+            },
+            priority: prio
+        },
+        token: registrationToken
+    };
+
+    return firebaseAdmin.messaging().send(message);
+}
